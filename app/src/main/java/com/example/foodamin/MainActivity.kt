bb1ced7ff89f371779565457eb6fd45da88ee64d
Lasse Kristensen <Lasse54.kristensen@live.dk>
2022-12-10 06:59:58 +0100

    setup the design again, as some pictures had to be seperated a bit more to allow for interaction. One layout got messed up but i will quickly fix that

--- app/src/main/java/com/example/foodamin/MainActivity.kt
+++ app/src/main/java/com/example/foodamin/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.foodamin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.room.Room
 import com.example.foodamin.databinding.ActivityMainBinding
 
 
@@ -15,8 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) // connects the binding variable to the layout file
         setContentView(binding.root) // set which layout file to view
-
-
 
         binding.btn.setOnClickListener{
             val barcodeScanning = Intent(this, BarcodeScanningActivity::class.java)
